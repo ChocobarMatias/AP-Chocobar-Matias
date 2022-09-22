abc74598ebe1d0cec22d61d268f8c71c1ff1a843
ChocobarMatias <chocobar_matias@yahoo.com.ar>
2022-09-22 05:44:25 -0300

    Front 4

--- PI-Chocobar-Matias/src/app/app.module.ts
+++ PI-Chocobar-Matias/src/app/app.module.ts
@@ -9,7 +9,9 @@ import { RedesComponent } from './components/redes/redes.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
-
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HysComponent } from './components/hys/hys.component';
 
 @NgModule({
   declarations: [
@@ -20,10 +22,13 @@ import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
     ExperienciaComponent,
     BannerComponent,
     AcercaDeComponent,
+    EducacionComponent,
+    HysComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
