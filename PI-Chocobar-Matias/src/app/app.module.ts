2ff75d84f24afb016a886dec25260400e290092d
ChocobarMatias <chocobar_matias@yahoo.com.ar>
2022-09-26 04:36:32 -0300

    Fin del Front

--- PI-Chocobar-Matias/src/app/app.module.ts
+++ PI-Chocobar-Matias/src/app/app.module.ts
@@ -5,25 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
-import { RedesComponent } from './components/redes/redes.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HysComponent } from './components/hys/hys.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoAPComponent,
-    RedesComponent,
     ExperienciaComponent,
     BannerComponent,
     AcercaDeComponent,
     EducacionComponent,
     HysComponent,
+    ProyectoComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
