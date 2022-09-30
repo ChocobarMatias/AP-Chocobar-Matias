69bc4174e6427c4e8afd69129b9f2208b4cd7c14
ChocobarMatias <chocobar_matias@yahoo.com.ar>
2022-09-30 04:03:55 -0300

    Carpeta BackEnd
    Creación de carpeta BackEnd

---
+++ PI-Chocobar-Matias/BackEnd/src/main/java/com/portfolio/matiassebastianchocobar/MatiasSebastianChocobarApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.matiassebastianchocobar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatiasSebastianChocobarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatiasSebastianChocobarApplication.class, args);
+	}
+
+}
---
+++ PI-Chocobar-Matias/BackEnd/src/test/java/com/portfolio/matiassebastianchocobar/MatiasSebastianChocobarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.matiassebastianchocobar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatiasSebastianChocobarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
