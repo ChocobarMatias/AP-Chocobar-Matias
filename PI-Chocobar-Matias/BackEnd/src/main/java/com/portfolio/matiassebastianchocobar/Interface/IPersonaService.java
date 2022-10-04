21ab3e28cd7d1a3354385821e3a85c3899867915
ChocobarMatias <chocobar_matias@yahoo.com.ar>
2022-10-04 04:25:29 -0300

    Trabajo en BackEnd

---
+++ PI-Chocobar-Matias/BackEnd/src/main/java/com/portfolio/matiassebastianchocobar/Interface/IPersonaService.java
@@ -0,0 +1,18 @@
+
+package com.portfolio.matiassebastianchocobar.Interface;
+
+import com.portfolio.matiassebastianchocobar.Entity.Persona;
+import java.util.List;
+
+
+public interface IPersonaService {
+
+ 
+    public List<Persona> getPersona();
+    
+    public void savePersona(Persona persona);
+    
+    public void deletePersona(Long id);
+    
+    public Persona findPersona(Long id);
+}
