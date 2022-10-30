34d970d1748af6afdca3a47462a34a7654fee592
ChocobarMatias <chocobar_matias@yahoo.com.ar>
2022-10-30 01:31:32 -0300

    detalle finales

--- PI-Chocobar-Matias/FrontEnd/src/app/app.module.ts
+++ PI-Chocobar-Matias/FrontEnd/src/app/app.module.ts
@@ -6,9 +6,9 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
-import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HysComponent } from './components/hys/hys.component';
@@ -17,21 +17,30 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { interceptorProvider } from './service/interceptor-service';
+import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
+import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
+import { NeweducacionComponent } from './components/educacion/neweducacion.component';
+import { EditeducacionComponent } from './components/educacion/editeducacion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoAPComponent,
-    ExperienciaComponent,
     BannerComponent,
     AcercaDeComponent,
+    ExperienciaComponent,
     EducacionComponent,
     HysComponent,
     ProyectoComponent,
     FooterComponent,
     HomeComponent,
     LoginComponent,
+    NewExperienciaComponent,
+    EditExperienciaComponent,
+    NeweducacionComponent,
+    EditeducacionComponent,
   ],
   imports: [
     BrowserModule,
@@ -41,7 +50,9 @@ import { LoginComponent } from './components/login/login.component';
     NgCircleProgressModule.forRoot({}),
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    interceptorProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- PI-Chocobar-Matias/FrontEnd/src/app/service/auth.service.ts
+++ PI-Chocobar-Matias/FrontEnd/src/app/service/auth.service.ts
@@ -9,7 +9,7 @@ import { NuevoUsuario } from '../model/nuevo-usuario';
   providedIn: 'root'
 })
 export class AuthService {
-  authURL = 'http://loalhost:8080/auth/';
+  authURL = 'https://backendmatias-sebastian-chocob.herokuapp.com/auth/';
 
   constructor(private httpClient: HttpClient) { }
 
