839bf87396274650e6ffb51add03a5cc32f75b96
ChocobarMatias <chocobar_matias@yahoo.com.ar>
2022-11-01 00:39:15 -0300

    carga 4

--- PI-Chocobar-Matias/BackEnd/src/main/java/com/portfolio/matiassebastianchocobar/Security/Controller/AuthController.java
+++ PI-Chocobar-Matias/BackEnd/src/main/java/com/portfolio/matiassebastianchocobar/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"https://frontendmatias-sebastian-choco.web.app","http://localhost:4200"})
+@CrossOrigin(origins = "https://frontendchocobarmatias.web.app")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
